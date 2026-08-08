b1a7a2b2e0de529f6c91bdf321f1510de620ac10
Vortextier <dhrubodhrubo282@gmail.com>
2026-08-08 23:42:29 +0600

    Update script.js

--- script.js
+++ script.js
@@ -55,8 +55,8 @@ const playersData = [
         tiers: [
             { icon: "https://i.postimg.cc/qM3XBvN6/nethop.png", label: "LT4" },
             { icon: "https://i.postimg.cc/xdK4XPqk/axe.png", label: "HT5" },
-            { icon: "https://i.postimg.cc/qB8vkfpb/sword.png", label: "HT1" },
-            { icon: "https://i.postimg.cc/sXG1PvdS/pot.png", label: "HT1" },
+            { icon: "https://i.postimg.cc/qB8vkfpb/sword.png", label: "?" },
+            { icon: "https://i.postimg.cc/sXG1PvdS/pot.png", label: "?" },
             { icon: "https://i.postimg.cc/zD9KQY0P/uhc.png", label: "LT4" },
             { icon: "https://i.postimg.cc/cL1RRMDy/smp.png", label: "LT5" },
             { icon: "https://i.postimg.cc/nLYDjsJS/638965736295609752.png", label: "LT5" },
