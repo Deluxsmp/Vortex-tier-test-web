5f82238b41927d2182490357a4d28b963bb50267
Vortextier <dhrubodhrubo282@gmail.com>
2026-08-09 09:02:03 +0600

    Update script.js

--- script.js
+++ script.js
@@ -16,6 +16,22 @@ const playersData = [
         ]
     },
     {
+        name: "SynThexOP",
+        points: 24,
+        region: "AS",
+        regionFull: "Asia",
+        tiers: [
+            { icon: "https://i.postimg.cc/qM3XBvN6/nethop.png", label: "LT4" },
+            { icon: "https://i.postimg.cc/xdK4XPqk/axe.png", label: "?" },
+            { icon: "https://i.postimg.cc/qB8vkfpb/sword.png", label: "LT4" },
+            { icon: "https://i.postimg.cc/sXG1PvdS/pot.png", label: "?" },
+            { icon: "https://i.postimg.cc/zD9KQY0P/uhc.png", label: "?" },
+            { icon: "https://i.postimg.cc/cL1RRMDy/smp.png", label: "HT4" },
+            { icon: "https://i.postimg.cc/nLYDjsJS/638965736295609752.png", label: "LT2" },
+            { icon: "https://i.postimg.cc/3x6kkgVP/mace.png", label: "HT5" }
+        ]
+    },
+    {
         name: "Itzsakib69",
         points: 30,
         region: "AS",
