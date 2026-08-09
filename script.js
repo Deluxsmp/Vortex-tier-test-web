d1788fe575001a085879ec8236655d813351e27d
Vortextier <dhrubodhrubo282@gmail.com>
2026-08-09 09:36:28 +0600

    Update script.js

--- script.js
+++ script.js
@@ -16,6 +16,22 @@ const playersData = [
         ]
     },
     {
+        name: "SR_Gamerzzz",
+        points: 2,
+        region: "AS",
+        regionFull: "Asia",
+        tiers: [
+            { icon: "https://i.postimg.cc/qM3XBvN6/nethop.png", label: "?" },
+            { icon: "https://i.postimg.cc/xdK4XPqk/axe.png", label: "?" },
+            { icon: "https://i.postimg.cc/qB8vkfpb/sword.png", label: "LT5" },
+            { icon: "https://i.postimg.cc/sXG1PvdS/pot.png", label: "?" },
+            { icon: "https://i.postimg.cc/zD9KQY0P/uhc.png", label: "?" },
+            { icon: "https://i.postimg.cc/cL1RRMDy/smp.png", label: "?" },
+            { icon: "https://i.postimg.cc/nLYDjsJS/638965736295609752.png", label: "?" },
+            { icon: "https://i.postimg.cc/3x6kkgVP/mace.png", label: "?" }
+        ]
+    },
+    {
         name: "ShreddedXD__",
         points: 3,
         region: "AS",
