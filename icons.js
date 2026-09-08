b36c710d1eaee4b204f044345467738c71ed0fb0
Vortextier <dhrubodhrubo282@gmail.com>
2026-09-08 22:46:15 +0600

    Update icons.js

--- icons.js
+++ icons.js
@@ -4,12 +4,13 @@
    Overall leaderboard rows, and the player details modal.
 */
 const VORTEX_ICONS = {
-  vanilla: '',
-  uhc: '',
-  pot: '',
-  nethop: '',
-  smp: '',
-  sword: '',
-  axe: '',
-  mace: ''
+  overall: 'https://mctiers.com/tier_icons/overall.svg',
+  vanilla: 'https://mctiers.com/tier_icons/vanilla.svg',
+  uhc: 'https://mctiers.com/tier_icons/uhc.svg',
+  pot: 'https://mctiers.com/tier_icons/pot.svg',
+  nethop: 'https://mctiers.com/tier_icons/nethop.svg',
+  smp: 'https://mctiers.com/tier_icons/smp.svg',
+  sword: 'https://mctiers.com/tier_icons/sword.svg',
+  axe: 'https://mctiers.com/tier_icons/axe.svg',
+  mace: 'https://mctiers.com/tier_icons/mace.svg'
 };
