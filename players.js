8522a1c69e87277ed1f7b576d779868c84daf8fa
Vortextier <dhrubodhrubo282@gmail.com>
2026-09-09 10:21:25 +0600

    Update players.js

--- players.js
+++ players.js
@@ -1,2 +1,51 @@
-/* VORTEX TIER — PLAYER DATA ONLY */
-const VORTEX_PLAYERS = [];
+/* =====================================================
+   VORTEX TIER â€” PLAYER DATA
+   Generated from Admin Panel
+   Replace ONLY your old players.js with this file.
+====================================================== */
+
+const VORTEX_PLAYERS = [
+  {
+    "id": 1788927665225,
+    "name": "Se9ku",
+    "region": "AS",
+    "skinUrl": "",
+    "namemc": "",
+    "results": {
+      "vanilla": {
+        "tier": "LT5",
+        "points": 5
+      },
+      "uhc": {
+        "tier": "",
+        "points": ""
+      },
+      "pot": {
+        "tier": "",
+        "points": ""
+      },
+      "nethop": {
+        "tier": "HT5",
+        "points": 14
+      },
+      "smp": {
+        "tier": "",
+        "points": ""
+      },
+      "sword": {
+        "tier": "",
+        "points": ""
+      },
+      "axe": {
+        "tier": "",
+        "points": ""
+      },
+      "mace": {
+        "tier": "",
+        "points": ""
+      }
+    },
+    "totalPoints": 19,
+    "position": 1
+  }
+];
